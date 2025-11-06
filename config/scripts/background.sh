d07f4cc71b048f3de4955d0147234bf87d22cad5
kurkum715 <kurkom4@gmail.com>
2025-11-07 00:24:32 +0800

    Update

---
+++ config/scripts/background.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+feh --bg-fill ~/Wallpapers/9.png &
---
+++ config/scripts/settings.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xinput set-prop "Logitech G102 LIGHTSYNC Gaming Mouse" "libinput Accel Profile Enabled" 0, 0, 1 &
+xinput set-prop "Logitech G102 LIGHTSYNC Gaming Mouse" "libinput Accel Speed" 0 &
